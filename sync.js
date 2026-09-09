0f4d9f6fd9555c77b0254961c37eae45742b14d2
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-09 09:08:28 +0200

    Imposta font Poppins

--- sync.js
+++ sync.js
@@ -1,4 +1,17 @@
 (() => {
+  const fontLink = document.createElement('link');
+  fontLink.rel = 'stylesheet';
+  fontLink.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap';
+  document.head.appendChild(fontLink);
+
+  const fontStyle = document.createElement('style');
+  fontStyle.textContent = `
+    html, body, body *, input, select, button, textarea {
+      font-family: 'Poppins', Arial, Helvetica, sans-serif !important;
+    }
+  `;
+  document.head.appendChild(fontStyle);
+
   const script = document.createElement('script');
   script.src = 'sync2.js?v=2';
   script.async = false;
