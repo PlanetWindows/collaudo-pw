6b6dce60f4a356e310425740af266c4f5e1d4361
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-11 09:56:28 +0200

    Forza nuova esportazione ZIP backend

--- operator-fix.js
+++ operator-fix.js
@@ -15,7 +15,7 @@
   document.head.appendChild(accessScript);
 
   const zipScript = document.createElement('script');
-  zipScript.src = 'zip-export.js?v=3';
+  zipScript.src = 'zip-export.js?v=4';
   zipScript.async = false;
   document.head.appendChild(zipScript);
 
