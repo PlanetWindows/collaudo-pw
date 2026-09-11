715815249756186e9dbfee269e799a3d59035cc8
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-11 10:05:19 +0200

    Forza nuova esportazione PDF nello ZIP

--- operator-fix.js
+++ operator-fix.js
@@ -15,7 +15,7 @@
   document.head.appendChild(accessScript);
 
   const zipScript = document.createElement('script');
-  zipScript.src = 'zip-export.js?v=4';
+  zipScript.src = 'zip-export.js?v=5';
   zipScript.async = false;
   document.head.appendChild(zipScript);
 
