f713e9c09ab5b06c9f076fc6089a6a8f4ad9a295
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-11 09:49:55 +0200

    Forza nuova versione esportazione ZIP

--- operator-fix.js
+++ operator-fix.js
@@ -15,7 +15,7 @@
   document.head.appendChild(accessScript);
 
   const zipScript = document.createElement('script');
-  zipScript.src = 'zip-export.js?v=1';
+  zipScript.src = 'zip-export.js?v=2';
   zipScript.async = false;
   document.head.appendChild(zipScript);
 
