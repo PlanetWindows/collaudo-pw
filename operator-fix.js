2f1a0b25c6af06d4dbc5a749d5bf386c171c05b9
PlanetWindows <planetwindowsmirandola@gmail.com>
2026-09-16 11:16:24 +0200

    Refresh production typology read-only cache version

--- operator-fix.js
+++ operator-fix.js
@@ -40,7 +40,7 @@
   document.head.appendChild(apolloSignatureScript);
 
   const officePrepProductionScript = document.createElement('script');
-  officePrepProductionScript.src = 'production-office-prep.js?v=1';
+  officePrepProductionScript.src = 'production-office-prep.js?v=2';
   officePrepProductionScript.async = false;
   document.head.appendChild(officePrepProductionScript);
 
